2d0e0de1c0b1c9bacb8d218251fab169eb86bb45
Tony Drake <t27duck@gmail.com>
2023-12-26 13:01:47 -0500

    Remove old Minitest compat

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,4 @@ require 'active_record'
 require 'active_support'
 require 'scopelist'
 
-# Compatability for older MiniTest versions (ie Rails 4.0 uses MiniTest 4.7)
-MiniTest::Test = MiniTest::Unit::TestCase unless defined?(MiniTest::Test)
-
 require 'models'
